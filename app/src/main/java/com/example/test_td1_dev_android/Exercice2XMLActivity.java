d6582d3005e353fa21b6e330c0e3fd4ef1c8f5e9
TechnoShark <mathieu.dasilva13@gmail.com>
2021-02-01 16:26:39 +0100

    Exercice 5 : Événements associés aux objets graphiques d’une vue Ajout d'une fenêtre de dialogue et changement de la couleur de fond lors de la confirmation.

--- app/src/main/java/com/example/test_td1_dev_android/Exercice2XMLActivity.java
+++ app/src/main/java/com/example/test_td1_dev_android/Exercice2XMLActivity.java
@@ -1,18 +1,24 @@
 package com.example.test_td1_dev_android;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Exercice2XMLActivity extends AppCompatActivity{
+public class Exercice2XMLActivity extends AppCompatActivity {
 
     Button confirmButton;
     EditText firstName, lastName;
+    Context context = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,7 +32,25 @@ public class Exercice2XMLActivity extends AppCompatActivity{
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello " + firstName.getText().toString() + " " + lastName.getText().toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "Hello " + firstName.getText().toString() + " " + lastName.getText().toString(), Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+
+                alertBuilder.setTitle("Confirmation");
+                alertBuilder.setCancelable(true).setMessage("Do you confirm your informations ?").
+                        setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                View mainLayout = findViewById(R.id.mainLayout);
+                                mainLayout.setBackgroundColor(Color.GREEN);
+                            }
+                        }).setNegativeButton("Change", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
             }
         });
     }
